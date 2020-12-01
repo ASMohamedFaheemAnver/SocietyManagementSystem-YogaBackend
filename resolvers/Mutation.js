aef1a03c06842d2e4469fc4f83d31cff2b25db70
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-12-01 17:17:29 +0530

    approveSociety and disApproveSociety resolvers added,

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,3 +1,52 @@
-const Mutation = {};
+const Society = require("../model/society");
+import getUserData from "../middleware/auth";
+
+const Mutation = {
+  approveSociety: async (parent, { societyId }, { request }, info) => {
+    const userData = getUserData(request);
+    if (!userData) {
+      const error = new Error("not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+
+    if (userData.category !== "developer") {
+      const error = new Error("only developer can approve societies!");
+      error.code = 401;
+      throw error;
+    }
+
+    if (!societyId) {
+      const error = new Error("invalid society id!");
+      error.code = 403;
+      throw error;
+    }
+    await Society.updateOne({ _id: societyId }, { $set: { approved: true } });
+    return { message: "approved successfly!" };
+  },
+
+  disApproveSociety: async (parent, { societyId }, { request }, info) => {
+    const userData = getUserData(request);
+    if (!userData) {
+      const error = new Error("not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+
+    if (userData.category !== "developer") {
+      const error = new Error("only developer can approve societies!");
+      error.code = 401;
+      throw error;
+    }
+
+    if (!societyId) {
+      const error = new Error("invalid society id!");
+      error.code = 403;
+      throw error;
+    }
+    await Society.updateOne({ _id: societyId }, { $set: { approved: false } });
+    return { message: "disapproved successfly!" };
+  },
+};
 
 export { Mutation as default };
