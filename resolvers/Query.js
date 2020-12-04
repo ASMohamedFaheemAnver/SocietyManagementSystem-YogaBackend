b1d07d7f0aa3cb81552d97cddf15b3ffe71525d1
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-12-04 09:59:13 +0530

    Login society,

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -80,6 +80,57 @@ const Query = {
     const societies = await Society.find();
     return societies;
   },
+  loginSociety: async (parent, { email, password }, { request }, info) => {
+    console.log({ email: email, password: password });
+
+    const errors = [];
+
+    if (!validator.isEmail(email)) {
+      errors.push({ message: "email is invalid!" });
+    }
+
+    if (password.length < 8) {
+      errors.push({
+        message: "password should be more than or equal to 8 charactors!",
+      });
+    }
+
+    if (errors.length > 0) {
+      const error = new Error("invalid credentials!");
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+
+    let society = await Society.findOne({ email: email });
+    if (!society) {
+      const error = new Error("society doesn't exist!");
+      error.code = 401;
+      throw error;
+    }
+
+    if (!society.approved) {
+      const error = new Error("society doesn't approved yet!");
+      error.code = 401;
+      throw error;
+    }
+
+    const isEqual = await bcrypt.compare(password, society.password);
+
+    if (!isEqual) {
+      const error = new Error("not authenticated!");
+      error.code = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      { encryptedId: society._id.toString(), category: "society" },
+      process.env.secret_word,
+      { expiresIn: "10h" }
+    );
+
+    return { token: token, _id: society._id.toString(), expiresIn: 36000 };
+  },
 };
 
 export { Query as default };
