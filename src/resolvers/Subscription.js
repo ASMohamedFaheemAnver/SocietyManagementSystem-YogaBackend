edce470df4291425751a8865fe6041fb7363ae46
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-12-09 15:06:34 +0530

    Socket for track,

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -8,7 +8,28 @@ const Subscription = {
       console.log({ emitted: "listenMemberLog" });
       const userData = getUserData(request);
       const member = await Member.findById(userData.encryptedId);
-      return pubSub.asyncIterator(`member:log:society(${member.society})`);
+      if (!member) {
+        const error = new Error("member doesn't exist!");
+        error.code = 403;
+        throw error;
+      }
+      return pubSub.asyncIterator(`member:log|society(${member.society})`);
+    }
+  },
+
+  listenMemberLogTrack: {
+    subscribe: async (parent, args, { request, pubSub }, info) => {
+      console.log({ emitted: "listenMemberLogTrack" });
+      const userData = getUserData(request);
+      const member = await Member.findById(userData.encryptedId);
+
+      if (!member) {
+        const error = new Error("member doesn't exist!");
+        error.code = 403;
+        throw error;
+      }
+
+      return pubSub.asyncIterator(`member:log:track|society(${member.society})&member(${member._id})`);
     }
   }
 };
